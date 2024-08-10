344ba8530a2037b793d8eb58270766e5fa2d244f
EntropyReversed <atila35743@gmail.com>
2024-08-10 03:31:57 +0200

    dots

--- PathAnimation.js
+++ PathAnimation.js
@@ -6,6 +6,7 @@ export class PathAnimation {
         this.svgWrap = this.wrap.querySelector('.path-animation__svg-wrap');
         this.scrollWrap = this.wrap.querySelector('.path-animation__scroll');
         this.svg = this.wrap.querySelector('svg');
+        this.ship = this.svg.querySelector('.path-animation__ship')
         this.timeline = gsap.timeline();
 
         this.init();
@@ -38,16 +39,32 @@ export class PathAnimation {
     }
 
     createTimeline() {
+        // const path = this.svg.querySelector('#path-animation__main-test');
+        // path.style.strokeDasharray = `200 ${path.getTotalLength() - 370}`;
+        // path.style.strokeDashoffset = path.getTotalLength();
+
+        gsap.set(this.wrap, {'--rotation': '70deg'})
         this.timeline.clear()
-        this.timeline
-            .set(this.wrap, {'--rotation': '55deg'})
-            .to(this.scrollWrap, {y: () => -(this.svgWrap.offsetHeight - window.innerHeight * 0.5), duration: 10, delay: 1, ease: 'none'}, 'start');
+            .to(this.scrollWrap, {y: () => -(this.svgWrap.offsetHeight - window.innerHeight * 0.5), duration: 10, delay: 2, ease: 'none'}, 'start')
+            // .to(path, { strokeDashoffset: 0, duration: 10, ease: 'none' }, 'start')
+            // .to(this.ship, {
+            //     motionPath: {
+            //         path: '#path-animation__main',
+            //         align: '#path-animation__main',
+            //         alignOrigin: [1, 0.5],
+            //         autoRotate: 0,
+            //     },
+            //     transformOrigin: "50% 50%",
+            //     duration: 10,
+            //     ease: 'none'
+            // }, 'start')
 
             this.segments.forEach((seg, i) => {
                 seg.style.strokeDasharray = seg.getTotalLength();
                 seg.style.strokeDashoffset = seg.getTotalLength();
 
-                this.timeline.to(seg, { strokeDashoffset: 0, delay: i * 2, duration: 2, ease: 'none' }, 'start');
+                this.timeline.to(seg, { strokeDashoffset: 0, delay: i * 2, duration: 2, ease: 'none' }, 'start')
+                .to(this.cardsInner[i], {opacity: 1, delay: 2, duration: 1, ease: 'none'}, '<')
             })
     }
 
@@ -64,6 +81,7 @@ export class PathAnimation {
 
     init() {
         this.cards = this.wrap.querySelectorAll('.path-animation__card');
+        this.cardsInner = this.wrap.querySelectorAll('.path-animation__card-wrap');
         this.anchors = this.svg.querySelectorAll('.path-animation__anchor');
         this.segments = this.svg.querySelectorAll('.path-animation__seg');
 
