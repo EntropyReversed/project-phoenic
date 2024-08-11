6456951dc4b58801a158c70ab35e3446c0e6cf10
EntropyReversed <atila35743@gmail.com>
2024-08-11 16:08:26 +0200

    started on responsive

--- PathAnimation.js
+++ PathAnimation.js
@@ -1,5 +1,3 @@
-// gsap.registerPlugin(ScrollTrigger) 
-
 const createHTMLElement = (tag, props) => {
 	const element = document.createElement(tag);
 	Object.keys(props).forEach((key) => {
@@ -10,159 +8,177 @@ const createHTMLElement = (tag, props) => {
 
 const createSVGElement = (tag, props) => {
 	const element = document.createElementNS("http://www.w3.org/2000/svg", tag);
-    Object.keys(props).forEach((key) => {
+	Object.keys(props).forEach((key) => {
 		element.setAttribute(key, props[key]);
 	});
 	return element;
 };
 
 export class PathAnimation {
-    angle = 70;
+	startAngle = 70;
+	angle = this.startAngle;
+	mobileBreak = 991;
 
-    constructor({wrap}) {
-        this.wrap = wrap;
-        this.svgWrap = this.wrap.querySelector('.path-animation__svg-wrap');
-        this.scrollWrap = this.wrap.querySelector('.path-animation__scroll');
-        this.svg = this.wrap.querySelector('svg');
-        this.ship = this.svg.querySelector('.path-animation__ship')
-        this.timeline = gsap.timeline();
+	constructor({ wrap }) {
+		this.wrap = wrap;
+		this.svgWrap = this.wrap.querySelector('.path-animation__svg-wrap');
+		this.scrollWrap = this.wrap.querySelector('.path-animation__scroll');
+		this.svg = this.wrap.querySelector('svg');
+		this.ship = this.svg.querySelector('.path-animation__ship')
+		this.timeline = gsap.timeline();
 
-        this.init();
-    }
+		this.isMobile = window.innerWidth < this.mobileBreak;
 
-    getPosition(item, target, point = { x: 0, y: 0 }) {
-        return MotionPathPlugin.convertCoordinates(
-            target,
-            item,
-            point
-        );
-    }
+		this.init();
+	}
 
-    positionCard(card, anchor, isLeft) {
-        const { x, y } = this.getPosition(card, anchor);
+	getPosition(item, target, point = { x: 0, y: 0 }) {
+		return MotionPathPlugin.convertCoordinates(
+			target,
+			item,
+			point
+		);
+	}
 
-        gsap.set(card, {
-            "--left": x,
-            "--top": y,
-        })
+	positionCard(card, anchor) {
+		const { x, y } = this.getPosition(card, anchor);
 
-        if (isLeft) {
-            gsap.set(card, {
-                xPercent: -100
-            })
-        }
-    }
+		gsap.set(card, {
+			"--left": x,
+			"--top": y,
+		})
+	}
 
-    positionCards() {
-        this.cards.forEach((card, i) => {
-            this.positionCard(card, this.anchors[i], i % 2 === 0)
-        });
-    }
+	positionCards() {
+		this.cards.forEach(card => {
+			card.classList.remove('isLeft');
+		});
 
-    positionLines() {
-        this.lines.forEach((line, i) => {
-            const target = this.linesTargets[i];
-            const { x, y } = this.getPosition(line, target);
+		this.cards.forEach((card, i) => {
+			this.positionCard(card, this.anchors[i]);
 
-            gsap.set(line, {
-                "--left": x,
-                "--top": y,
-            })
-        })
-    }
+			if (i % 2 === 0) {
+				card.classList.add('isLeft');
+			}
+		});
+	}
 
-    createLines() {
-        this.lines = [];
-        this.linesTargets = [];
-        this.linesPerSeg = 3;
-        this.segments.forEach((seg, i) => {
-            Array.from({length: this.linesPerSeg}).forEach((_, line) => {
-                const len = seg.getTotalLength() / this.linesPerSeg;
-                const isMain = i !== 0 && len * line < len;
-                const offset = isMain ? 0 : (Math.random() - 1) * len * 0.1;
-                const sizeOffset = Math.random() * (1 - 0.5) + 0.5;
-                const pos = seg.getPointAtLength((len + offset) * line);
+	positionLines() {
+		this.lines.forEach((line, i) => {
+			const target = this.linesTargets[i];
+			const { x, y } = this.getPosition(line, target);
 
-                if (i === 0 && line === 0) return;
+			gsap.set(line, {
+				"--left": x,
+				"--top": y,
+			})
+		})
+	}
 
-                const target = createSVGElement('circle', {
-                    cx: pos.x,
-                    cy: pos.y+3,
-                    r: 1,
-                    fill: "transparent",
-                });
-                
-                const lineEl = createHTMLElement('div', {
-                    className: `path-animation__line ${isMain ? 'main' : ''}`,
-                });
+	createLines() {
+		this.lines = [];
+		this.linesTargets = [];
+		this.linesPerSeg = 3;
+		this.segments.forEach((seg, i) => {
+			Array.from({ length: this.linesPerSeg }).forEach((_, line) => {
+				const len = seg.getTotalLength() / this.linesPerSeg;
+				const isMain = i !== 0 && len * line < len;
+				const offset = isMain ? 0 : (Math.random() - 1) * len * 0.1;
+				const sizeOffset = Math.random() * (1 - 0.5) + 0.5;
+				const pos = seg.getPointAtLength((len + offset) * line);
 
-                lineEl.style.setProperty('--size-off', sizeOffset)
+				if (isMain || (i === 0 && line === 0)) return;
 
-                this.svg.appendChild(target);
-                this.scrollWrap.appendChild(lineEl);
+				const target = createSVGElement('circle', {
+					cx: pos.x,
+					cy: pos.y + 3,
+					r: 1,
+					fill: "transparent",
+				});
 
-                this.linesTargets.push(target);
-                this.lines.push(lineEl);
-            })
-        })
-    }
+				const lineEl = createHTMLElement('div', {
+					className: `path-animation__line ${isMain ? 'main' : ''}`,
+				});
 
-    createTimeline() {
-        gsap.set(this.wrap, {'--rotation': 0})
-        this.timeline.clear()
-            .to(this.scrollWrap, {y: () => -(this.svgWrap.offsetHeight - window.innerHeight * 0.5), duration: 10, delay: 2, ease: 'none'}, 'start')
-            .to(this.wrap, {'--rotation': this.angle, duration: 4, delay: 0}, 'start')
-            this.segments.forEach((seg, i) => {
-                seg.style.strokeDasharray = seg.getTotalLength();
-                seg.style.strokeDashoffset = seg.getTotalLength();
+				lineEl.style.setProperty('--size-off', sizeOffset)
 
-                this.timeline.to(seg, { strokeDashoffset: 0, delay: i * 2, duration: 2, ease: 'none' }, 'start')
+				this.svg.appendChild(target);
+				this.scrollWrap.appendChild(lineEl);
 
-                if (this.cardsInner[i]) {
-                    this.timeline.to(this.cardsInner[i], {opacity: 1, delay: 2, duration: 1, ease: 'none'}, '<')
-                }
-            })
-    }
+				this.linesTargets.push(target);
+				this.lines.push(lineEl);
+			})
+		})
+	}
 
-    resetAnimation() {
-        gsap.set(this.wrap, { '--rotation': 0 });
-        gsap.set(this.scrollWrap, { y: 0 })
-        gsap.set(this.cards, {
-            "--left": 0,
-            "--top": 0,
-        });
-        gsap.set(this.lines, {
-            "--left": 0,
-            "--top": 0,
-        });
-    }
+	createTimeline() {
+		gsap.set(this.wrap, { '--rotation': 0 })
+		this.timeline.clear()
+			.to(this.scrollWrap, { y: () => -(this.svgWrap.offsetHeight - window.innerHeight * 0.5), duration: 10, delay: 2, ease: 'none' }, 'start')
+			.to(this.wrap, { '--rotation': this.angle, duration: 4 }, 'start')
+			.to(this.lines, { opacity: 0.5, duration: 4 }, 'start');
 
-    init() {
-        this.cards = this.wrap.querySelectorAll('.path-animation__card');
-        this.cardsInner = this.wrap.querySelectorAll('.path-animation__card-wrap');
-        this.anchors = this.svg.querySelectorAll('.path-animation__anchor');
-        this.segments = this.svg.querySelectorAll('.path-animation__seg');
+		this.segments.forEach((seg, i) => {
+			seg.style.strokeDasharray = seg.getTotalLength();
+			seg.style.strokeDashoffset = seg.getTotalLength();
 
-        this.createLines();
-        this.positionCards();
-        this.positionLines();
-        this.createTimeline();
+			this.timeline.to(seg, { strokeDashoffset: 0, delay: i * 2, duration: 2, ease: 'none' }, 'start')
 
-        this.scrollTrigger = ScrollTrigger.create({
-            trigger: this.wrap,
-            start: 'top center',
-            end: 'bottom bottom',
-            // markers: true,
-            scrub: 1,
-            animation: this.timeline,
-            invalidateOnRefresh: true,
-        })
+			if (this.cardsInner[i]) {
+				this.timeline.to(this.cardsInner[i], { opacity: 1, delay: 2, duration: 1, ease: 'none' }, '<')
+			}
+		})
+	}
 
-        window.addEventListener('resize', () => {
-            this.resetAnimation();
-            this.positionCards();
-            this.positionLines();
-            this.createTimeline();
-        })
-    }
+	resetAnimation() {
+		this.isMobile = window.innerWidth < this.mobileBreak;
+
+		if (this.isMobile) {
+			this.angle = 50;
+		} else {
+			this.angle = this.startAngle;
+		}
+
+		gsap.set(this.wrap, { '--rotation': 0 });
+		gsap.set(this.scrollWrap, { y: 0 })
+		gsap.set(this.cards, {
+			"--left": 0,
+			"--top": 0,
+		});
+		gsap.set(this.lines, {
+			"--left": 0,
+			"--top": 0,
+		});
+	}
+
+	init() {
+		this.cards = this.wrap.querySelectorAll('.path-animation__card');
+		this.cardsInner = this.wrap.querySelectorAll('.path-animation__card-wrap');
+		this.anchors = this.svg.querySelectorAll('.path-animation__anchor');
+		this.segments = this.svg.querySelectorAll('.path-animation__seg');
+
+		this.createLines();
+		this.positionCards();
+		this.positionLines();
+		this.createTimeline();
+
+		this.scrollTrigger = ScrollTrigger.create({
+			trigger: this.wrap,
+			start: 'top center',
+			end: 'bottom bottom',
+			markers: true,
+			scrub: 1,
+			animation: this.timeline,
+			invalidateOnRefresh: true,
+		})
+
+		gsap.to(this.wrap, { opacity: 1 })
+
+		window.addEventListener('resize', () => {
+			this.resetAnimation();
+			this.positionCards();
+			this.positionLines();
+			this.createTimeline();
+		})
+	}
 }
